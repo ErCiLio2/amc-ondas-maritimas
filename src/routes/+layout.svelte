1315771c2682b70ab58f3b2b168dd388a10c780f
Ercilio Julio <erciliociu@gmail.com>
2026-08-26 02:06:22 +0100

    chore(ux): threshold scroll reveal 0.1 → 0.15

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -38,7 +38,7 @@
 						}
 					});
 				},
-				{ threshold: 0.1, rootMargin: '0px 0px -40px 0px' }
+				{ threshold: 0.15, rootMargin: '0px 0px -40px 0px' }
 			);
 
 			targets.forEach((el, i) => {
